e88ae2b98f2e4dee99ec7dfe670a4c9d2c79f37e
amonroca <amonroca@gmail.com>
2024-01-26 00:24:01 -0300

    w03 - encapsulation design activity

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,20 @@
+public class Reference
+{
+    private string _book;
+    private int _chapter, _verse, _endVerse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+
+    }
+
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+}
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,25 @@
+public class Scripture
+{
+    private Reference reference;
+    private List<Word> _words;
+
+    Scripture(Reference reference, string text)
+    {
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return false;
+    }
+}
