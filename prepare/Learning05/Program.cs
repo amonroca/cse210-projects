8cd78aa4a5594b09328488a4da95ebd56865fdf2
amonroca <amonroca@gmail.com>
2024-02-07 23:26:14 -0300

    w05 - learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("blue", 2.2);
+        // Console.WriteLine($"square color: {square.GetColor()} square area: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("red", 5, 3.5);
+        // Console.WriteLine($"rectangle color: {rectangle.GetColor()} rectangle area: {rectangle.GetArea()}");
+
+        Circle circle = new Circle ("yellow", 6.3);
+        // Console.WriteLine($"circle color: {circle.GetColor()} circle area: {circle.GetArea()}");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea()}.");
+        }
     }
 }
